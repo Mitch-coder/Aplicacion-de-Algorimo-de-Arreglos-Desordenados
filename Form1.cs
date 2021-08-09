d49c4da522ea3d723a62e1c89e7379f9199a1336
Mitch-coder <michellescalderon2002@gmail.com>
2021-08-08 21:00:01 -0500

    funcionalidad 1 beta

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,8 @@ namespace applicacionDeAlgoritmoUno
 {
     public partial class Form1 : Form
     {
-        Persona[] persona;
+        Persona objPersona = new Persona();
+        Persona []arrayPersona;
         int size,position=0,i,x;
         public Form1()
         {
@@ -21,8 +22,10 @@ namespace applicacionDeAlgoritmoUno
         private void btnSet_Click(object sender, EventArgs e)
         {
             size = int.Parse(textQuantity.Text);
-            persona = new Persona[size];
+            arrayPersona = new Persona[size];
             position = 0;
+            clearText(1);
+            dtgvRegister.Rows.Clear();
         }
         private void btnCreate_Click(object sender, EventArgs e)
         {
@@ -30,65 +33,104 @@ namespace applicacionDeAlgoritmoUno
         }
         private void btnRead_Click(object sender, EventArgs e)
         {
-            search("No encontrado");
+            if (search("No fue encontrado ID:") == true)
+            {
+                dtgvRegister.Rows.Clear();
+                dtgvRegister.Rows[0].Cells[0].Value = arrayPersona[i].id;
+                dtgvRegister.Rows[0].Cells[1].Value = arrayPersona[i].name;
+                dtgvRegister.Rows[0].Cells[2].Value = arrayPersona[i].age;
+            }
         }
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            if(search("No existe") == true)
+            if(search("No existe ID:") == true)
             {
-                persona[i].name = textName.Text;
-                persona[i].age = int.Parse(textAge.Text);
-                MessageBox.Show("La persona con Id ="+ x +"se actualizo");
+                arrayPersona[i].name = textName.Text;
+                arrayPersona[i].age = int.Parse(textAge.Text);
+                MessageBox.Show("La persona con Id = "+ x +" se actualizo");
             }
         }
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            if (search("No existe") == true)
+            if (search("No existe ID:") == true)
             {
-                for (int k = i; k <= position - 1; k++)
+                for (int k = i; k <position-1; k++)
                 {
-                    persona[k].id = persona[k + 1].id;
-                    persona[k].name = persona[k + 1].name;
-                    persona[k].age = persona[k + 1].age;
+                    arrayPersona[k] = arrayPersona[k+1];
                 }
                 position--;
-                MessageBox.Show("La persona con id" + x + "se elimino");
+                MessageBox.Show("La persona con id " + x + " se elimino");
             }
         }
         private void btnShow_Click(object sender, EventArgs e)
         {
-            for(int i =0;i<position;i++)
+            dtgvRegister.Rows.Clear();
+            for(int i =0;i<arrayPersona.Length;i++)
             {
                 dtgvRegister.Rows.Add();
-                dtgvRegister.Rows[i].Cells[0].Value = persona[i].id;
-                dtgvRegister.Rows[i].Cells[1].Value = persona[i].name;
-                dtgvRegister.Rows[i].Cells[2].Value = persona[i].age;
+                dtgvRegister.Rows[i].Cells[0].Value = arrayPersona[i].id;
+                dtgvRegister.Rows[i].Cells[1].Value = arrayPersona[i].name;
+                dtgvRegister.Rows[i].Cells[2].Value = arrayPersona[i].age;
             }
         }
         // funciones para validar espacio y buscar
         public void insert()
         {
-            if(position<=size-1)
-                persona[position].Create(int.Parse(textId.Text),textName.Text,int.Parse(textAge.Text));
+            if (position <= size - 1)
+            {
+                try
+                {
+                    objPersona.Create(int.Parse(textId.Text), textName.Text, int.Parse(textAge.Text));
+                    arrayPersona[position] = objPersona;
+                    position++;
+                    objPersona = new Persona();
+                    clearText(1);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Todos los campos son requeridos");
+                }
+            }
             else
+            {
                 MessageBox.Show("No hay espacio");
+                clearText(2);
+            }
+               
         }
         public bool search(string mensaje)
         {
             i = 0;
-            x = int.Parse(textId.Text);
-
-            while (i < position && x != persona[position].id)
+            try
             {
-                i++;
+                x = int.Parse(textId.Text);
+                while (i < position && x != arrayPersona[i].id)
+                {
+                    i++;
+                }
+                if (i >= position)
+                {
+                    MessageBox.Show(mensaje + x);
+                    return false;
+                }
+                else
+                    return true;
             }
-            if (i >= position)
+            catch (Exception)
             {
-                MessageBox.Show(x+mensaje);
-                return false;
+                MessageBox.Show("Es necesario el ID del usuario");
             }
+            return false;
+        }
+        public void clearText(int focus)
+        {
+            textId.Clear();
+            textName.Clear();
+            textAge.Clear();
+            if (focus == 1)
+                textId.Focus();
             else
-                return true;
+                textQuantity.Focus();
         }
     }
 }
